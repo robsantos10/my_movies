6eb239e12e853542467f8d0e2f5d1db8eb4257fc
ruby 16 <ruby.16@metralha.cec.ime.usp.br>
2017-01-26 21:42:19 -0200

    Criacao das Views e Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  #get 'movies/index'
+  get 'movies' => "movies#index"
+
+  #get 'movies/new'
+  get 'movies/new' => "movies#new"
+
+  #get 'movies/show'
+  get 'movies/:id' => "movies#show"
+
+  #get 'movies/create'
+  post 'movies' => "movies#create"
+
+  get 'movies/:id/edit' => "movies#edit"
+
+  put 'movies/:id/' => "movies#update"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
