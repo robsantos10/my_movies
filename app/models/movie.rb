151906fb60c5919cd5edfcbe25b211aa0938dc0a
ruby 16 <ruby.16@metralha.cec.ime.usp.br>
2017-01-30 21:48:31 -0200

    Inserção das classificações de filmes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,4 +6,16 @@ class Movie < ActiveRecord::Base
 
   belongs_to :director, class_name: 'Director', foreign_key: "director_id", optional: true
   has_and_belongs_to_many :actors, associtation_foreign_key: 'actor_id', join_table: 'actors_movies'
+
+  has_many :classifications
+  has_many :users, through: :classifications
+
+  def average_score
+    score = classifications.average(:score)
+    if score.nil?
+      return nil
+    else
+      return score.to_f
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,7 @@ Rails.application.routes.draw do
 
   root 'movies#index'
 
+  post 'movies/:id/classifications' => "movies#create_classification"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
